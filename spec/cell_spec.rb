78c93f33c0b47caf4d43f9973917fa16f581db72
Riley McCullough <rileybmccullough@gmail.com>
2022-07-12 12:36:27 -0600

    Test: can place ship on cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -25,4 +25,9 @@ RSpec.describe Cell do
     expect(@cell.empty?).to eq(true)
   end
 
+  it 'can place ship on cell' do
+    @cell.place_ship(@cruiser)
+    expect(@cell.ship.name).to eq("cruiser")
+    expect(@cell.empty?).to eq(false)
+  end
 end
