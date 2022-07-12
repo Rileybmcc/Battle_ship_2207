feb87be2f9a696357b7cf12e2f560f1774b85e9c
Erik Riggs <riggs.erik@gmail.com>
2022-07-11 22:28:03 -0600

    feat: ship can be hit

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,14 +1,19 @@
 class Ship
   attr_reader :name,
               :length,
-              :health,
+              # :health,
               :number_of_hits
 
   def initialize(name, length)
     @name = name
     @length = length
     @number_of_hits = 0
-    @health = @length - @number_of_hits
+    # @health = @length - @number_of_hits
+
+  end
+
+  def health
+    @length - @number_of_hits
 
   end
 
@@ -19,6 +24,6 @@ class Ship
 
   def hit
     @number_of_hits += 1
-
+    
   end
 end
