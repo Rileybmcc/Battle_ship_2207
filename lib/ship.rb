654195188cc49cdc8862d6b2e59aca3918872164
Erik Riggs <riggs.erik@gmail.com>
2022-07-11 21:29:34 -0600

    feat: can tell if ship has sunk

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,16 +1,17 @@
 class Ship
   attr_reader :name,
-              :length
+              :length,
+              :health
 
   def initialize(name, length)
     @name = name
     @length = length
-
+    @health = @length
 
   end
 
-  def health
-    @length 
-  end
+  def sunk?
+    @health == 0
 
+  end
 end
