01948c2e2f833f332ae9b5ba354fcf645f93e605
Erik Riggs <riggs.erik@gmail.com>
2022-07-11 20:58:03 -0600

    Test: it can have instance of ship

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,5 +1,10 @@
 require "./lib/ship"
 
 RSpec.describe Ship do
+  it 'can have instance of ship' do
+    cruiser = Ship.new('cruiser', 3)
+
+    expect(cruiser).to be_instance_of(Ship)
+  end
 
 end
