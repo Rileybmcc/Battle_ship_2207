62fd7245305176d9401259695ca7f20d12e94b4d
Riley McCullough <rileybmccullough@gmail.com>
2022-07-14 20:30:33 -0600

    Test: can render cells that have been fired upon

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -46,7 +46,12 @@ RSpec.describe Board do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     sub = Ship.new("Submarine", 2)
-    board.place(cruiser, ["A1", "A2" "A3"])
+
+    expect(board.cells["A1"].empty?).to eq(true)
+    expect(board.cells["A2"].empty?).to eq(true)
+    expect(board.cells["A3"].empty?).to eq(true)
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
+    board.place(cruiser, ["A1", "A2", "A3"])
     expect(board.cells["A1"].ship == board.cells["A3"].ship).to eq(true)
 
   end
@@ -55,7 +60,8 @@ RSpec.describe Board do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     sub = Ship.new("Submarine", 2)
-    board.place(cruiser, ["A1", "A2" "A3"])
+    expect(board.valid_placement?(cruiser, ["A1", "B1", "C1"])).to eq(true)
+    board.place(cruiser, ["A1", "A2", "A3"])
     expect(board.valid_placement?(sub, ["A1", "B1"])).to eq(false)
 
   end
@@ -65,9 +71,31 @@ RSpec.describe Board do
     cruiser = Ship.new("Cruiser", 3)
     sub = Ship.new("Submarine", 2)
 
-    board.place(cruiser, ["A1", "A2" "A3"])
-    expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+    expect(board.cells["B1"].empty?).to eq(true)
+    expect(board.cells["B2"].empty?).to eq(true)
+    expect(board.cells["B3"].empty?).to eq(true)
 
+    expect(board.valid_placement?(cruiser, ["B1", "B2", "B3"])).to eq(true)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+    expect(board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
   end
 
+  it 'can render cells that have been fired upon' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    sub = Ship.new("Submarine", 2)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+    board.place(sub, ["C1", "D1"])
+
+    board.cells["A1"].fire_upon
+    board.cells["B4"].fire_upon
+    board.cells["C1"].fire_upon
+    board.cells["D1"].fire_upon
+
+    expect(board.render(true)).to eq("  1 2 3 4 \nA H S S . \nB . . . M \nC X . . . \nD X . . . \n")
+  end
 end
